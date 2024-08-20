3efea25baa447b4ce4f1037af752e415b279be6c
Frank Rodríguez López <fr20587@gmail.com>
2024-08-20 15:08:01 -0400

    quitando hydration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -129,7 +129,7 @@ export function HttpLoaderFactory(http: HttpClient) {
             withFetch(),
             withInterceptorsFromDi()
         ),
-        provideClientHydration(),
+        // provideClientHydration(),
         CookieService,
     ],
     bootstrap: [AppComponent]
